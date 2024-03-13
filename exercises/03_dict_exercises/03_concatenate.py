a780d13c0773a4c95239812b67b5f276dd136ea3
wooihaw <wooihaw.tan@gmail.com>
2024-03-13 17:02:36 +0800

    Updated at the end of Day 2

--- exercises/03_dict_exercises/03_concatenate.py
+++ exercises/03_dict_exercises/03_concatenate.py
@@ -5,3 +5,13 @@
 d1 = {'a': 10, 'b': 20}
 d2 = {'c': 30, 'd': 40}
 d3 = {'e': 50}
+
+# Method 1 - Use the union operator
+d4_1 = d1 | d2 | d3
+print(f"{d4_1 = }")
+
+# Method 2 - Use the update() method
+d4_2 = {}
+for d in (d1, d2, d3):
+    d4_2.update(d)
+print(f"{d4_2 = }")
--- exercises/03_dict_exercises/05_frequency.py
+++ exercises/03_dict_exercises/05_frequency.py
@@ -1,2 +1,17 @@
 # Write a Python script to find the frequency of occurance for each alphabet in a string.
 astring = 'The quick brown fox jumps over the lazy dog.'
+
+# Clean and normalize the string
+t = [c.lower() for c in astring if c.isalpha()]
+
+# Method 1 - Use the for loop
+freq1 = {}
+for c in sorted(set(t)):
+    freq1[c] = t.count(c)
+print(f"{freq1 = }")
+
+# Method 2 - dictionary comprehension
+freq2 = {c: t.count(c) for c in sorted(set(t))}
+print(f"{freq2 = }")
+
+print(f"The letter 'o' appears {freq2['o']} times in the string.")
